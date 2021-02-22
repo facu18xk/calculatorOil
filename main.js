8967bde1f4e3693920e4a84a70a81d244e4d4ab7
Facu <5675299@mec.edu.py>
2021-02-22 14:23:45 -0300

    finish the first calc

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-//formula por cada 10 litros 200ml
+const calcular = () => {
+    let litros = document.querySelector(".nafta").value;
+    if(litros === ""){
+        document.querySelector(".result").innerHTML = "Por favor ingrese un numero"
+    }else{
+    let n = parseInt(litros);
+    let mult = 2 * n;
+    let re = mult / 100;
+    document.querySelector(".result").innerHTML = `Hay que utilizar ${re} cc para una mezcla perfecta`
+    }
+}
+
